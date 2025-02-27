6fe14e7154bff8624af5c039d7ea17dce2f0ab76
bBob21 <102711034+bBob21@users.noreply.github.com>
2025-02-27 18:06:11 +1100

    Added button to change grid dimensions and fixed asthetics
    Button can now change the grid dimensions within fixed grid size and fixed border lines

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 let container = document.querySelector("#container");
-const GRID_DIMENSIONS = 16;
+let grid_dimensions = 16;
 const GRID_WIDTH = 840;
 
-for (let i = 1; i < GRID_DIMENSIONS**2+1; i++){
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.style["width"] = `${GRID_WIDTH/GRID_DIMENSIONS}px`;
-    square.style["height"] = `${GRID_WIDTH/GRID_DIMENSIONS}px`;
-
-    square.addEventListener("mouseover",() => {
-        square.classList.add("draw");
-    })
-
-    container.appendChild(square);
+function setDimensions(){
+    grid_dimensions = parseInt(prompt("Enter new side length between 0 and 100:"));
+    if (grid_dimensions < 100 && grid_dimensions > 0){
+        deleteOldGrid();
+        createGrid(grid_dimensions);
+    }
+    else{
+        alert("ERROR! Invalid input!")
+    }
 }
 
+function deleteOldGrid(){
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+}
+
+function createGrid(){
+    for (let i = 1; i < grid_dimensions**2+1; i++){
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.style["width"] = `${GRID_WIDTH/grid_dimensions}px`;
+        square.style["height"] = `${GRID_WIDTH/grid_dimensions}px`;
+
+        square.addEventListener("mouseover",() => {
+            square.classList.add("draw");
+        })
+
+        container.appendChild(square);
+    }
+}
+
+createGrid(grid_dimensions);
